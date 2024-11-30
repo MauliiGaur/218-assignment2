fd64fc024eaa4fb7b33bca49a8286357902b26c2
MauliiGaur <mauli.gaur@edu.sait.ca>
2024-11-30 16:58:11 -0700

    made a webpage that is functional and responds to the buttons upon clicking and shows different information

---
+++ script.js
@@ -0,0 +1,44 @@
+intro.addEventListener("click", function(){
+  document.getElementById("image").src = 
+  "images/intro.jpg";
+  document.getElementById("subheader").textContent =
+  "Introduction";
+  document.getElementById("description").textContent =
+  "Here you will learn about the Monarch butterfly life cycle. They can be distinguished by thier unique markings and orange and black coloring. They are an endangered species of butterfly due to various factors such as climate change and habitat destruction. Click through the buttons to learn more about thier lifecycle."
+ })
+
+stage1.addEventListener("click", function(){
+  document.getElementById("image").src = 
+  "images/stage1.jpg";
+  document.getElementById("subheader").textContent =
+ "Egg Stage";
+  document.getElementById("description").textContent =
+  "Monarch butterfly eggs are laid during the Spring and Summer seasons on the underside of a specific plant called the Milkweed plant. The eggs are laid white in color and turn black or grey as they are about to hatch.It takes about 3-8 days for these eggs to hatch. These eggs are so small it is said that they can fit through the top of a needle! In the image these eggs look like clear water beads."
+ })
+
+ stage2.addEventListener("click", function(){
+  document.getElementById("image").src = 
+  "images/stage2.jpg";
+  document.getElementById("subheader").textContent =
+ "Larva Stage";
+  document.getElementById("description").textContent =
+  "Once the eggs are hatched, each larva goes through 5 stages of growth, molting after each one. After each stage the larva molts into a larger version of itself storing energy from the Milkweed in the form of fat and nutrients, to help it through the pupal stage. As a defence mechanism caterpillars exclusively eat the milkweed plant as it produces a milky toxin that is distasteful to predators. This toxin is stored in the body of the larva. At the end of all 5 stages of growth the larva is almost 2 inches in length!"
+ })
+
+ stage3.addEventListener("click", function(){
+  document.getElementById("image").src = 
+  "images/stage3.jpg";
+  document.getElementById("subheader").textContent =
+ "Chrysalis Stage";
+  document.getElementById("description").textContent =
+  "The fully grown larva then looks for a pupation site and attaches itself to a horizontal surface using a silk pad, upside down in the shape of a letter J. In this position the larva sheds its skin, leaving itself encased in its green exoskeleton. During this pupal stage, the adult butterfly forms inside. At first the chrysalis is opaque green, and becomes transparent after about 2 weeks before the butterfly is ready to emerge."
+ })
+
+ stage4.addEventListener("click", function(){
+  document.getElementById("image").src = 
+  "images/stage4.jpg";
+  document.getElementById("subheader").textContent =
+ "Adult Stage";
+  document.getElementById("description").textContent =
+  "Once hatched the butterfly takes time to dry out its wings and pump fluid back into them to further expand and stiffen them. Once the butterfly is ready it flies off to find nectar. During warm summers the life cycle of a butterfly can happen within 25 days and extend to a few weeks during the springtime."
+ })
